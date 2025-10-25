50f0790c8eb2e70ef1d18e788e6e6ebacf170617
empowrapp08162025 <empowrapp08162025@gmail.com>
2025-10-24 22:10:17 -0400

    fix: Add missing DisclaimerBanner import to justice-as-a-service

--- app/(tabs)/resources/justice-as-a-service.tsx
+++ app/(tabs)/resources/justice-as-a-service.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Alert, ScrollView, StyleSheet, Text, View } from "react-native";
 
 import A11yPressable from "../../../components/A11yPressable";
+import DisclaimerBanner from "../../../components/DisclaimerBanner";
 import { HIT_SLOP_8 } from "../../../constants/A11Y";
 import { MAX_FONT_SCALE, useAnnounceOnMount, useFocusOnRefOnMount } from "../../../hooks/useA11y";
 import { useTranslation } from "../../../i18n";
